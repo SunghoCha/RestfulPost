7c1439df1afcebb1ce6beb9004aade7023575f42
SunghoCha <tjdgh3725@gmail.com>
2024-06-24 22:05:40 +0900

    RESTful Blog - 사용자 등록 API 구현
    -post 방식으로 등록하는 메서드 구현 (테스트 없는 간이 버전)

--- src/main/java/com/sh/post/controller/UserController.java
+++ src/main/java/com/sh/post/controller/UserController.java
@@ -3,9 +3,7 @@ package com.sh.post.controller;
 import com.sh.post.bean.User;
 import com.sh.post.dao.UserDaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,10 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return userDaoService.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
     }
 }
