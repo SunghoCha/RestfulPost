0aa3b53ff2382ea76491321446c3f60d7dd8d7c7
SunghoCha <tjdgh3725@gmail.com>
2024-06-25 14:13:29 +0900

    RESTful Blog - filter를 이용한 사용자권한에 따른 조회 분기 간이 구현
    -jsonFilter를 활용해서 접속 url을 달리 했을때 보여주는 데이터 다르게 보여줌

---
+++ src/main/java/com/sh/post/controller/AdminUserController.java
@@ -0,0 +1,49 @@
+package com.sh.post.controller;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import com.sh.post.bean.AdminUser;
+import com.sh.post.bean.User;
+import com.sh.post.dao.UserDaoService;
+import com.sh.post.exception.UserNotFoundException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminUserController {
+    private final UserDaoService userDaoService;
+
+    // /admin/users/{id}
+    // /admin/users
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser); // user에서 adminUser로 복사
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+        return mapping;
+    }
+
+}
