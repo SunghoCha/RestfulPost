3f718a71dd1217655bab93f8e471a052935a00e7
SunghoCha <tjdgh3725@gmail.com>
2024-06-25 16:57:53 +0900

    RESTful Blog - Media type 정보를 이용한 버전관리
    - Media type에 버전정보 표시해서 변경된 코드 관리하는 방법 간단 구현
    *
    (일반 브라우저에서 실행 가능)
    URI versioning : Twitter (= X)
    Request Parameter versioning : Amazon
    
    (일반 브라우저에서 실행 불가)
    (Custom) headers versioning : Microsoft
    Media type versioning : GitHub

--- src/main/java/com/sh/post/controller/AdminUserController.java
+++ src/main/java/com/sh/post/controller/AdminUserController.java
@@ -31,7 +31,8 @@ public class AdminUserController {
     // /admin/users
     //@GetMapping("/v1/users/{id}")
     //@GetMapping(value = "/users/{id}", params = "version=1")
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
@@ -77,7 +78,8 @@ public class AdminUserController {
     // -> /admin/v2/users/{id}
     //@GetMapping("/v2/users/{id}")
     //@GetMapping(value = "/users/{id}", params = "version=2")
-    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUuser4AdminV2(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
