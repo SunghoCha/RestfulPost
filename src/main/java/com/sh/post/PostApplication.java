8772f6afba273b1933c0e4ed2486e9637bb9b949
SunghoCha <tjdgh3725@gmail.com>
2024-06-25 10:26:57 +0900

    RESTful Blog - 간단한 다국어 처리 로직 작성
    -messages.properties를 활용한 간단한 다국어 처리
    *글로벌 서비스를 만들 일은 거의 없어서 추후에 messageConverter
    원리 정도 파악하는정도로 하고 실제로 쓸 일은 크게 없을 것 같음

--- src/main/java/com/sh/post/PostApplication.java
+++ src/main/java/com/sh/post/PostApplication.java
@@ -3,6 +3,11 @@ package com.sh.post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class PostApplication {
@@ -14,6 +19,13 @@ public class PostApplication {
 //		for (String beanDefinitionName : beanDefinitionNames) {
 //			System.out.println("beanDefinitionName = " + beanDefinitionName);
 //		}
+	}
+
+	@Bean // SessionLocaleResolver는 생소한 클래스
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
