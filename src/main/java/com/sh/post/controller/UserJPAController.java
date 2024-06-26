1f3735c837bad8312a855e500b35e73c31ffee7b
SunghoCha <tjdgh3725@gmail.com>
2024-06-26 11:10:07 +0900

    RESTful Blog - JPA를 이용한 사용자 추가와 삭제 구현
    - 사용자 추가, 삭제 로직 구현
    *응답객체가 아닌 엔티티를 그대로 반환해서 유연성 떨어지는 상태로 간이 구현

--- src/main/java/com/sh/post/controller/UserJPAController.java
+++ src/main/java/com/sh/post/controller/UserJPAController.java
@@ -1,18 +1,20 @@
 package com.sh.post.controller;
 
 import com.sh.post.bean.User;
+import com.sh.post.dao.UserDaoService;
 import com.sh.post.exception.UserNotFoundException;
 import com.sh.post.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,11 +31,17 @@ public class UserJPAController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+        /*
+            리스트 형태로 반환하면 유연성 떨어짐. 만약 전체사용자수와 같이 출력해야 한다고하면 수정 어려움
+            응답 데이터에 맞는 객체 만들고 그 일부로서 유저 리스트 넣을것.
+            여기선 Service 계층이 없지만 총 사용자수를 넣는다면 유저리스트와 사용자수 쿼리를 트랜잭션(query니까 read-only로 하면 될 듯) 안에서 처리 한 후 DTO에 넣어서 반환하도록 해서
+            Controller가 entity에 대해 모르도록 하는게 나을지도? 
+         */
     }
 
     // /jpa/users/{id}
     @GetMapping("/users/{id}")
-    public ResponseEntity retrieveUserById(@PathVariable Integer id) {
+    public ResponseEntity retrieveUserById(@PathVariable int id) {
         Optional<User> user = userRepository.findById(id);
 
         if(user.isEmpty()) {
@@ -44,5 +52,26 @@ public class UserJPAController {
         WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         userEntityModel.add(linkBuilder.withRel("all-users")); // all-users -> http://localhost:8088/users
         return ResponseEntity.ok(userEntityModel);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    // jpa/users
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        // 현재 요청 기반 URI 구성 
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        // WebMvcLinkBuilder를 이용한 URI 구성
+        URI uri = linkTo(methodOn(this.getClass()).retrieveUserById(savedUser.getId())).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
