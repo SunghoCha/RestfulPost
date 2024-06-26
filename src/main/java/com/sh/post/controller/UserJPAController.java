03a46e46e43a71f7cfb968650612c8e16fad8943
SunghoCha <tjdgh3725@gmail.com>
2024-06-26 10:16:58 +0900

    RESTful Blog - JPA를 이용한 개별 사용자 상세 조회
    -권한이 있는 사용자가 조회했을시 uri 정보를 담아서 유저정보와 함께 반환하도록 구현

--- src/main/java/com/sh/post/controller/UserJPAController.java
+++ src/main/java/com/sh/post/controller/UserJPAController.java
@@ -1,13 +1,23 @@
 package com.sh.post.controller;
 
 import com.sh.post.bean.User;
+import com.sh.post.exception.UserNotFoundException;
 import com.sh.post.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -19,5 +29,20 @@ public class UserJPAController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    // /jpa/users/{id}
+    @GetMapping("/users/{id}")
+    public ResponseEntity retrieveUserById(@PathVariable Integer id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isEmpty()) {
+            throw new UserNotFoundException("id -" + id);
+        }
+        EntityModel<User> userEntityModel = EntityModel.of(user.get());
+
+        WebMvcLinkBuilder linkBuilder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        userEntityModel.add(linkBuilder.withRel("all-users")); // all-users -> http://localhost:8088/users
+        return ResponseEntity.ok(userEntityModel);
     }
 }
